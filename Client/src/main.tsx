32b3ed822cb4d20f5bb436d099ffac23deb6d852
Denilson Rabelo <denilsonrabelo.dev@gmail.com>
2025-03-22 21:14:51 -0300

    :sparkles: feat: Implementa página de post com roteamento e estilização, além de melhorias no componente de blog

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import App from './App';
+import DashboardLayoutBasic from './Layout';
+import Blog from './pages/Blog';
+import MembersPage from './pages/Members/MembersPage';
+import PostPage from './pages/Post';
+const router = createBrowserRouter([
+  {
+    Component: App,
+    children: [
+      {
+        path: '/',
+        Component: DashboardLayoutBasic,
+        children: [
+          {
+            path: '/liArb-academy',
+            Component: Blog,
+          },
+          {
+            path: '/estrutura',
+            Component: MembersPage,
+          },
+          {
+            path: '/liArb-academy/post/:id',
+            Component: PostPage,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
